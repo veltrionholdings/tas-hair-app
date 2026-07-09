4bb996dcae567ea84d3eaa0298c1d1ec8abbd111
Thabo Tsotetsi <tsotetsithabo36@gmail.com>
2026-07-09 09:50:45 +0200

    profile page api field fix

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
-import { api, logout, getStoredEmail, getUserRole, isAuthenticated, getCustomerId, Customer } from '../api/client';
+import { logout, getStoredEmail, getUserRole, isAuthenticated, getCustomerId, Customer } from '../api/client';
 import ConfirmModal from '../components/ConfirmModal';
 import './ProfilePage.css';
 
